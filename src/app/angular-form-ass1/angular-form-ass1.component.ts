7e623b0d7e50bc1fd541dedd6f0dda55eac55140
swathi-mindfire <swathim@mindfiresolutions.com>
2021-03-20 03:09:04 -0700

    Template driven form

--- src/app/angular-form-ass1/angular-form-ass1.component.ts
+++ src/app/angular-form-ass1/angular-form-ass1.component.ts
@@ -12,10 +12,57 @@ export class AngularFormAss1Component implements OnInit {
   fname = "";
   password = "";
   password2 = "";
+  countries :any[] ;
+  selectedCountryCode1:any;
+  states:any;
+  selectedState1:any;
+  selectedCountryCode2:any;
+  num1 :number;
+  num2 :number;
+  operation:any;
+  expr :string;
+  operations :any;
+  captchaResult :number;
+  op :any;
+
+
+  
+  selectedState2:any;
+ 
+  //fnStatus :boolean =false ;
 
   constructor() { 
     this.show = "grp1";
     this.model ={};
+    this.countries =  [
+                      {name:"Australia" , code:"AUS"},
+                      {name :"America", code:"US" },
+                      {name:"Canada", code:"CA"},
+                      {name :"India", code:"IN"},
+                      {name:"Romainia", code:"RO"},
+                      {name: "Russia",code:"RU"},
+                      {name:"Switzerland",code:"SW"}
+                    ];
+    this. selectedCountryCode1= null;
+    this.selectedState1 = null;
+    this. selectedCountryCode2= null;
+    this.selectedState2 = null;
+    this.states = { "AUS": ["Australian Capital Territory","New South Wales","Northern Territory","Queensland","South","Australia","Tasmania","Victoria","Western Australia"],
+                    "US" : ["Alabama.Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","New York", "Wisconsin","Wyoming"],
+                    "IN" : ["Andhra Pradesh","Arunachal Pradesh","Assam,Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Odisha"],
+                    "RO" : ["Alba","Arad","Arges","Bacau","Bihor","Bistrita-Nasaud","Botosani","Braila","Brasov","Bucuresti","Buzau"],
+                    "RU" : ["Adygeya","Aginskiy Buryatskiy","Altay","Altayskiy","Amurskaya","Arkhangel","skaya","Astrakhanskaya","Bashkortostan"],
+                    "SW" : ["Aargau","Ausser-Rhoden","Basel-Landschaft","Basel-Stadt","Bern","Fribourg","Geneve","Glarus","Graubunden","Inner-Rhoden","Jura,Luzern","Neuchatel"]
+                    
+    }
+    this.num1= Math.round((100-10)*Math.random() +10 );
+    
+    this.num2= Math.round((100-10)*Math.random() + 10);
+    this.operations = ['*','/','+','-'];
+    this.expr = "";
+    this.captchaResult =0;
+    this.op ="";
+
   }
 
   ngOnInit(): void {
@@ -31,6 +78,21 @@ export class AngularFormAss1Component implements OnInit {
   showGrp1(){
     this.show = "grp1";
   }
+  genCaptcha(){ 
+    this. num1     =  Math.round((100-10)*Math.random() +10 );
+    this.num2      =  Math.round((100-10)*Math.random() + 10);
+    let x          =  (Math.round(10*Math.random()))%4;
+    this .op        =  this.operations[x]; 
+    this.expr      =  this.num1 + this.op  + this.num2
+}
+validateCaptcha(){
+  let temp = this.num1 + this.op +this.num2
+  alert(temp)
+
+}
+
+  
+
   /*
   validateForm(regForm :any){
     console.log("submitted" +regForm);
@@ -39,6 +101,7 @@ export class AngularFormAss1Component implements OnInit {
   save(s :any){
     console.log(s)
   }
+  /*
   matchPasswords(){{}
     if(this.password !=this.password2){
       return true;
@@ -46,7 +109,15 @@ export class AngularFormAss1Component implements OnInit {
     else{
       return false;
     }
-  }
+  }*/
+  /*checkGrp1(regForm:any){
+   
+    this.fnStatus=regForm.controls.fname.status;
+    console.log(this.fnStatus)
+    return this.fnStatus;
+
+
+  }*/
 
 }
 
